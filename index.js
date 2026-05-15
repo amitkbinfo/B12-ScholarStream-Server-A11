c7d0da84f3ab77e2876d3e139ef791662722c8fa
Amit KB <mr.amitkb@gmail.com>
2026-05-15 20:42:00 +0600

    Initial setup for ScholarStream server (A11)

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`ScholerSteam is running on port: ${port}`);
+    console.log(`ScholarSteam is running on port: ${port}`);
 })
