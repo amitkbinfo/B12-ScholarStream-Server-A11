b69263f07a32267521f5a1d5de852def392dcc53
Amit KB <mr.amitkb@gmail.com>
2026-05-15 20:41:19 +0600

    Initial setup for ScholarStream server (A11)

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("ScholarStream is connected!");
+})
+
+app.listen(port, () => {
+    console.log(`ScholerSteam is running on port: ${port}`);
+})
